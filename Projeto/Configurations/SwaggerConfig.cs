92b39c75dce8d8c831e1daf1069e123bcd5227a6
GEORGENIL ANTONIO DA SANTOS <ge.creed@gmail.com>
2025-02-11 09:09:50 -0300

    Configuração para swagger

--- Projeto/Configurations/SwaggerConfig.cs
+++ Projeto/Configurations/SwaggerConfig.cs
@@ -11,6 +11,31 @@ namespace Projeto.Configurations
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Projeto", Version = "v1" });
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please enter token",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    BearerFormat = "JWT",
+                    Scheme = "bearer"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type=ReferenceType.SecurityScheme,
+                                Id="Bearer"
+                            }
+                        },
+                        new string[]{}
+                    }
+                });
             });
         }
 
