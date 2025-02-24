887bbd9d752139d618c8799457c3387b836caee8
GEORGENIL ANTONIO DA SANTOS <ge.creed@gmail.com>
2025-02-24 14:37:44 -0300

    Inicio de estruturação para upload de arquivos.

---
+++ Projeto.Domain/Models/Arquivo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto.Domain.Models
+{
+    public class Arquivo : Base
+    {
+        public string Name { get; set; }
+        public string Extensao { get; set; }
+        public byte[] Bytes { get; set; }
+    }
+}
---
+++ Projeto/Controllers/FileController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Projeto.Domain.Models;
+
+namespace Projeto.Controllers
+{
+    [Route("file")]
+    [ApiController]
+    public class FileController : ControllerBase
+    {
+        [HttpPost("upload")]
+        public async Task<ActionResult> Upload([FromForm] ICollection<IFormFile> files)
+        {
+            if (files == null || files.Count == 0)
+                return BadRequest();
+
+            List<Arquivo> arquivos = new();
+
+            foreach (IFormFile formFile in files)
+            {
+                if (formFile.Length > 0)
+                {
+                    using (var stream = new MemoryStream())
+                    {
+                        await formFile.CopyToAsync(stream);
+
+                        var arquivo = new Arquivo();
+
+                        arquivo.Bytes = stream.ToArray();
+                        arquivo.Name = formFile.FileName;
+                        arquivo.Extensao = Path.GetExtension(formFile.Name);
+
+                        arquivos.Add(arquivo);
+                    }
+                }
+            }
+
+            //chamar repositório para persistir
+
+            return Ok();
+        }
+    }
+}
